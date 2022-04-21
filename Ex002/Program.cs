a0fb31ce1b61470f843723ffdb5e29025ec8b99e
Vasilii <bespalchuk1985@gmail.com>
2022-04-21 18:39:22 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число: ");
+int Number =  Convert.ToInt32(Console.ReadLine());
+
+if (Number / 100 > 0)
+{
+    Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 1 % 10}");
+}
+else 
+{
+    Console.WriteLine($"У числа {Number} нет третьей цифры");
+}
+
